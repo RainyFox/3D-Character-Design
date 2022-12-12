bb57b639e4ce443a0548c83aadc47a1c6d544661
RainyFox <karta035865@gmail.com>
2022-12-12 20:36:45 +0800

    Add empty weapon damage script, fix light leak <22 00:04:52>

---
+++ Assets/Scripts/WeaponDamage.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDamage : MonoBehaviour
+{
+    [SerializeField] float damage = 398;
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
